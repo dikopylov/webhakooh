1406e028096c7c658177c70e9aa356956d64bc81
AAArmand <armand.airapetyan@yandex.ru>
2018-11-11 21:06:17 +0500

    menu init

--- resources/views/administration/home.blade.php
+++ resources/views/administration/home.blade.php
@@ -10,11 +10,6 @@
                         как {{ $user->roles()->pluck('name')->implode(' ') }}</div>
 
                     <div class="card-body">
-                        @if (session('status'))
-                            <div class="alert alert-success" role="alert">
-                                {{ session('status') }}
-                            </div>
-                        @endif
 
                         @if ($user->hasRole('Администратор'))
 
