9393c274e203c15c8cda89c2d06877e8b38677d6
AAArmand <armand.airapetyan@yandex.ru>
2019-01-19 16:15:32 +0500

    выпилил мусорный роут

--- routes/api.php
+++ routes/api.php
@@ -12,11 +12,6 @@ use Illuminate\Http\Request;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
-
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
-});
-
 Route::post('get-contacts', 'API\ContactController@show');
 
 Route::post('get-scheme', 'API\SchemeController@show');
